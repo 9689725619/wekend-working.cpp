115e527dbe4685875dabff659dcb56b00da9c5d2
Shubhangi meshram <101330420+9689725619@users.noreply.github.com>
2022-03-29 17:22:31 +0530

    Add files via upload

---
+++ wekend ,working.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+char day[3];
+char strcmp;
+printf("enter any day :");
+gets(day);
+if strcmp(day,"sat")==0||strcmp(day,"sun")==0
+
+
+{
+	printf("wekeend")
+}
+else
+{
+	printf("working ");
+}
+return 0;
+}
